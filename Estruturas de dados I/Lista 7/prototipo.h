9d3f8706903fb216dba9fe4f5930c7448dc47bff
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-06-01 15:34:44 -0300

    Update prototipo.h

--- Estruturas de dados I/Lista 7/prototipo.h
+++ Estruturas de dados I/Lista 7/prototipo.h
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char* ler();
+
+int tamanhoStr(char *str);
+int comparar(char *str1, char *str2);
+char* concatenar(char *str1, char *str2);
+void liberar(char *str);
