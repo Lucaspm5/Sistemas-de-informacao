a5d587a989d7f7b4be250798a0d99b11297657ee
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-06-13 11:03:03 -0300

    Update 9.c

--- Estruturas de dados I/Lista 2/9.c
+++ Estruturas de dados I/Lista 2/9.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
-#include <stdilib.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-    int **A = NULL;
+int main() {
+    int** matriz;
 
-    A = (int**)malloc(3 * sizeof(int*));
+    matriz = (int**)malloc(3 * sizeof(int*));
 
-    for (int i = 0;i < 3;i++)
-        *(A + i) = (int*)malloc(3 * sizeof(int));
+    for (int i = 0;i < 3;i++) matriz[i] = (int*)malloc(3 * sizeof(int));
 
-    for (int i = 0;i < 3;i++){
-        for (int j = 0;j < 3;j++)
-            printf("[%p]",&*(*(A + i) + j));
+    for (int i = 0;i < 3;i++) {
+        for (int j = 0;j < 3;j++) {
+            *(*(matriz + i) + j) = rand();
+        }
+    }
+
+    for (int i = 0;i < 3;i++) {
+        for (int j = 0;j < 3;j++) {
+            printf("%p ", &*(*(matriz + i)+ j));
+        }
         printf("\n");
     }
 
-    for (int i = 0;i < 3;i++)
-        free(*(A + i));
+    for (int i = 0;i < 3;i++) free(matriz[i]);
+
+    free(matriz);
 
     return 0;
 }
