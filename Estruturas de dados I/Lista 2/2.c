90abf6d2f3752d4c34058f32811ff36b8efc5bf5
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-06-13 10:35:02 -0300

    Update 2.c

--- Estruturas de dados I/Lista 2/2.c
+++ Estruturas de dados I/Lista 2/2.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-int main()
-{
-    int a,b;
-
-    bool auxiliary = (&a > &b);
-
-    if(auxiliary) printf("%p",&a);
-    if(!auxiliary) printf("%p",&b);
-
+int main() {
+    int a, b;
+    (&a > &b) ? printf("%p\n", &a) : printf("%p\n", &b);
     return 0;
 }
